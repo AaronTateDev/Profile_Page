9a1965a8b1e2a71d7d36d6f090698dbfc54054bb
Aaron Tate <aaronjtate@gmail.com>
2019-07-23 18:50:35 -0500

    Updating project to work on iphone

--- css+js/port-main.js
+++ css+js/port-main.js
@@ -14,3 +14,19 @@ function navMenu() {
 
 $('a[href="#"], a[href="#mProjects"], a[href="#mSkills"], a[href="#aMe"], a[href="#cMe"], .icon').
 attr('onclick', "navMenu()");//Hide dropdown on select
+
+document.addEventListener("touchend", function (event) {
+
+  if (event.target.parentElement.id == 'project1') {
+    window.location.href = "projects.html#project1";
+  }
+
+   if (event.target.parentElement.id == 'project2') {
+    window.location.href = "projects.html#project2";
+  }
+
+   if (event.target.parentElement.id == 'project3') {
+    window.location.href = "projects.html#project3";
+  }
+
+}, false);
